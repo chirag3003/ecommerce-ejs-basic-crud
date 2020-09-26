cfdc5dfdd859aeb45369ddd3d9fddfd341c3ce4a
chirag3003 <chiragbhalotia0412@gmail.com>
2020-09-26 21:37:55 +0530

    first time hosting

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set("view engine" , "ejs");
+
+var prod = [];
+
+
+app.get("/",function(req,res){
+	res.render("index",{prod:prod});
+})
+
+app.get("/admin/admin",function(req,res){
+	res.render("admin",{});
+})
+
+app.post("/admin/product",function(req,res){
+	var data = req.body;
+	var product = {
+		title:data.title,
+		des:data.des,
+	}
+	prod.push(product);
+	res.render("admin",{})
+
+})
+
+
+
+
+
+
+app.listen(3000,function(){
+	console.log("server started on port 3000");
+})
